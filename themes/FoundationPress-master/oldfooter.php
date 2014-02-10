d14b2f073cc4b76f319362a64a5d70856ca8e3f5
uriahharrell <uriahharrell@gmail.com>
2014-02-10 12:07:55 -0800

    Test

---
+++ themes/FoundationPress-master/oldfooter.php
@@ -0,0 +1,45 @@
+</div>
+</section>
+
+     <div class="footer large-12 columns">
+        <div class="row">
+
+          <div class="footer-logo large-2 large-offset-1 columns">
+            <img src="wp-content/themes/matchfc/_/inc/images/footer-logo.png" alt="MatchFC: Match Fixture Connection">
+          </div>
+
+          <div class="footer-nav large-4 columns">
+            <ul class="large-6 columns">
+              <li><a href="#">About us</a></li>
+              <li><a href="#">How it works</a></li>
+              <li><a href="#">More information</a></li>
+              <li><a href="#">Something else</a></li>
+            </ul>
+            <ul class="large-6 columns">
+              <li><a href="#">Login</a></li>
+              <li><a href="#">My account</a></li>
+              <li><a href="#">Make a game</a></li>
+              <li><a href="#">Search for games</a></li>
+            </ul>
+          </div>
+
+          <div class="footer-contact large-4 columns">
+            <div class="row">
+              <ul>
+                <li class="large-3"><a href="#"><img src="" alt=""></a></li>
+                <li class="large-3"><a href="#"></a></li>
+                <li class="large-3"><a href="#"></a></li>
+                <li class="large-3"><a href="#"></a></li>
+                <li class="large-3"><a href="#"></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+    </div>
+</div>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
